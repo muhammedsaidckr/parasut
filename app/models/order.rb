8ee28e9d3fd51f0b9e16d877a10cefc34e603aec
Muhammed Said Cakir <muhammedsaidckr@gmail.com>
2022-01-13 00:04:58 +0300

    Order modeli icin validationlar tamamlandi, net_amount icin before_create ile net tutar hesaplamasi yapilarak atandi

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,16 @@
 class Order < ApplicationRecord
+  validates :currency_type, presence: true
+  validates :gross_amount, presence: true, numericality: true, comparison: { greater_than: :discount_amount }
+  validates :discount_amount, numericality: true
+
+  belongs_to :category, class_name: "Category", optional: true
+  enum currency_type: {
+    TL: 0,
+    USD: 1,
+    EUR: 2
+  }
+
+  before_create do
+    self.net_amount = self.gross_amount - self.discount_amount
+  end
 end
