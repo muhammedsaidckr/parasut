5711d4cc0380621d917c125479af7f15d1a3c9a3
Muhammed Said Cakir <muhammedsaidckr@gmail.com>
2022-01-15 02:14:47 +0300

    Before validation callback oncesi strip yapildi.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,10 @@
 class Category < ApplicationRecord
   validates :name, presence:true, uniqueness: true
+
+  before_validation :strip_whitespace
+
+  private
+  def strip_whitespace
+    self.name = self.name.strip unless self.name.nil?
+  end
 end
