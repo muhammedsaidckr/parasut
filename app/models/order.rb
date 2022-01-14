99b17d184980345ab8b2086d161a5a3631a741be
Muhammed Said Cakir <muhammedsaidckr@gmail.com>
2022-01-15 02:16:38 +0300

    Validasyonlar duzeltildi

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   validates :currency_type, presence: true
-  validates :gross_amount, presence: true, numericality: true
-  validates :discount_amount, allow_nil: true,  numericality: true, comparison: { less_than: :gross_amount }
+  validates :gross_amount, presence: true, numericality: { :greater_than => 0 }
+  validates :discount_amount, allow_nil: true,  numericality: true, comparison: { less_than: :gross_amount, greater_than_or_equal_to: 0 }
 
   belongs_to :category, class_name: "Category", optional: true
   enum currency_type: {
@@ -14,6 +14,7 @@ class Order < ApplicationRecord
     if self.discount_amount
       self.net_amount = self.gross_amount - self.discount_amount
     else
+      self.discount_amount = 0.0
       self.net_amount = self.gross_amount
     end
   end
